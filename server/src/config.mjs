5db4f594cdfa487e6eff3fc8e1ca235e918c93bc
NarniaEXE <122504749+NarniaEXE@users.noreply.github.com>
2026-09-13 16:18:14 +0200

    Update config.mjs

--- server/src/config.mjs
+++ server/src/config.mjs
@@ -13,6 +13,7 @@ export const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
 export const DISCORD_GUILD_ID = process.env.DISCORD_GUILD_ID;
 export const DISCORD_VERIFIED_ROLE_ID = process.env.DISCORD_VERIFIED_ROLE_ID;
 export const DISCORD_LOG_CHANNEL_ID = process.env.DISCORD_LOG_CHANNEL_ID;
+export const DASHBOARD_PASSWORD = process.env.DASHBOARD_PASSWORD;
 
 export const SELF_APP_NAME =
   process.env.SELF_APP_NAME || "Self Discord Verification Hardcoded";
